2d2b59627c59e9549d46321413a2f1ca4dd699bb
sAMs126 <492457689@qq.com>
2019-04-09 17:51:04 +0800

    缩进

--- day05/webpack-study/src/main.js
+++ day05/webpack-study/src/main.js
@@ -4,8 +4,8 @@
 import $ from 'jquery'
 
 $(function () {
-    $('li:odd').css('backgroundColor','yellow')
-    $('li:even').css('backgroundColor',function () {
+    $('li:odd').css('backgroundColor', 'yellow')
+    $('li:even').css('backgroundColor', function () {
         return '#' + 'D97634'
     })
 })
